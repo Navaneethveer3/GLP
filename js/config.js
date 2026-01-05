e8c3cd775c9b33e70f8a8017593d29060eb8fe3c
Navaneethveer3 <navaneeth42.com@gmail.com>
2026-01-05 20:56:08 +0530

    Add config.js for deployment (force)

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// This file is ignored by Git to prevent API key leaks.
+const firebaseConfig = {
+    apiKey: "AIzaSyCjCJAXzCzDYGpZaS544Y9VnBowWqIQyo4",
+    authDomain: "gl-platform-3b824.firebaseapp.com",
+    projectId: "gl-platform-3b824",
+    storageBucket: "gl-platform-3b824.firebasestorage.app",
+    messagingSenderId: "548293664159",
+    appId: "1:548293664159:web:73946cd5cf2e910046f1aa",
+    measurementId: "G-WQE9661CD8"
+};
+
+window.firebaseConfig = firebaseConfig;
